46b1d93690680ffc23083026f3c4583f6b56aefb
MaximillianStewart <mstewart@ucsb.edu>
2023-04-24 15:32:57 -0700

    added articleFixtures page

---
+++ frontend/src/fixtures/articleFixtures.js
@@ -0,0 +1,37 @@
+const articleFixtures = {
+    oneArticle:
+    [
+      {
+       "id": 1,
+        "title": "Santa Barbara Pet Resources",
+        "image": "https://www.edhat.com/sites/default/files/news_image/marley_3.png",
+        "content": "Santa Barbara Humane has partnered with FreeWill to offer free, quick, and accessible estate planning services",     
+      }
+    ],
+
+    threeRestaurants:
+    [
+        {
+            "id": 2,
+             "title": "Space X Rocket",
+             "image": "https://media.cnn.com/api/v1/images/stellar/prod/230420091549-03-spacex-starship-launch-0420.jpg?c=16x9&q=h_720,w_1280,c_fill/f_webp",
+             "content": "SpaceX’s Starship, the most powerful rocket ever built, took off from a launchpad in South Texas at 9:33 a.m. ET Thursday but exploded midair before stage separation.",      
+        },
+
+        {
+            "id": 3,
+             "title": "Deltopia 2023",
+             "image": "https://nypost.com/wp-content/uploads/sites/2/2022/04/California.jpeg?resize=1024,1536&quality=75&strip=all",
+             "content": "Deltopia weekend returned in full force this year, as an estimated crowd of 15,000 descended on the streets of Isla Vista for the annual unsanctioned street festival.",
+        },
+
+        {
+            "id": 4,
+             "title": "Biden 2024",
+             "image": "https://s.abcnews.com/images/US/president-biden-rt-jef-230420_1682027106505_hpMain_16x9_992.jpg",
+             "content": "Biden speaks on announcing his candidacy for the 2024 presidential election",
+        }, 
+    ]
+};
+
+export { restaurantFixtures };
