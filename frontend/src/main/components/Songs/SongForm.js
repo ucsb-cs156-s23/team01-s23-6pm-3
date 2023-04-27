71b8004b2d4e61bd94e298ed62cfa17513de53e2
Esme Puzio <esmepuzio@csilvm-08.cs.ucsb.edu>
2023-04-27 11:15:06 -0700

    First Pass, haven't tested

---
+++ frontend/src/main/components/Songs/SongForm.js
@@ -0,0 +1,143 @@
+import React from 'react' //imports react framework, redundant
+import { Button, Form } from 'react-bootstrap'; //does what it says
+import { useForm } from 'react-hook-form' //import useForm hook -> library to provide vaildation for data entry
+import { useNavigate } from 'react-router-dom'; //use to navigate to other pages
+
+function SongForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+
+    const navigate = useNavigate();
+    
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+    // Stryker enable all
+   
+    const testIdPrefix = "SongForm";
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="id">Id</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-id"}
+                        id="id"
+                        type="text"
+                        {...register("id")}
+                        value={initialContents.id}
+                        disabled
+                    />
+                </Form.Group>
+            )}
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="title">Name</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-title"}
+                    id="title"
+                    type="text"
+                    isInvalid={Boolean(errors.name)}
+                    {...register("title", {
+                        required: "Title is required.",
+                        maxLength : {
+                            value: 50,
+                            message: "Max length 50 characters"
+                        }
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.name?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="artist">Description</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-artist"}
+                    id="artist"
+                    type="text"
+                    isInvalid={Boolean(errors.description)}
+                    {...register("artist", {
+                        required: "Artist is required.",
+                        maxLength : {
+                            value: 50,
+                            message: "Max length 50 characters"
+                        }
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.description?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="description">Description</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-description"}
+                    id="description"
+                    type="text"
+                    isInvalid={Boolean(errors.description)}
+                    {...register("description", {
+                        required: "Description is required."
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.description?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="album">Description</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-album"}
+                    id="album"
+                    type="text"
+                    isInvalid={Boolean(errors.description)}
+                    {...register("album")}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.description?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="year-released">Description</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-year-released"}
+                    id="year-released"
+                    type="text"
+                    isInvalid={Boolean(errors.description)}
+                    {...register("year-released")}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.description?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Button
+                type="submit"
+                data-testid={testIdPrefix + "-submit"}
+            >
+                {buttonLabel}
+            </Button>
+            <Button
+                variant="Secondary"
+                onClick={() => navigate(-1)}
+                data-testid={testIdPrefix + "-cancel"}
+            >
+                Cancel
+            </Button>
+
+        </Form>
+
+    )
+}
+
+export default SongForm;
