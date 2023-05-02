9609eacaf80bd3413e19aa45c53033bf9932c52d
MrJacobEisner <mrjeisner@gmail.com>
2023-05-01 17:24:31 -0700

    je - fixed broken test

--- frontend/src/tests/components/Movies/MovieForm.test.js
+++ frontend/src/tests/components/Movies/MovieForm.test.js
@@ -8,18 +8,18 @@ import { QueryClient, QueryClientProvider } from "react-query";
 
 const mockedNavigate = jest.fn();
 
-jest.mock("react-router-dom", () => ({
-    ...jest.requireActual("react-router-dom"),
-    useNavigate: () => mockedNavigate,
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
 }));
 
 describe("MovieForm tests", () => {
     const queryClient = new QueryClient();
 
-    const expectedHeaders = ["Name", "Starring", "Director"];
+    const expectedHeaders = ["Name","Starring","Director"];
     const testId = "MovieForm";
 
-    test("renders correctly with no initialDirectors", async () => {
+    test("renders correctly with no initialContents", async () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
@@ -33,14 +33,15 @@ describe("MovieForm tests", () => {
         expectedHeaders.forEach((headerText) => {
             const header = screen.getByText(headerText);
             expect(header).toBeInTheDocument();
-        });
+          });
+
     });
 
-    test("renders correctly when passing in initialDirectors", async () => {
+    test("renders correctly when passing in initialContents", async () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <MovieForm initialDirectors={movieFixtures.oneMovie} />
+                    <MovieForm initialContents={movieFixtures.oneMovie} />
                 </Router>
             </QueryClientProvider>
         );
@@ -56,6 +57,7 @@ describe("MovieForm tests", () => {
         expect(screen.getByText(`Id`)).toBeInTheDocument();
     });
 
+
     test("that navigate(-1) is called when Cancel is clicked", async () => {
         render(
             <QueryClientProvider client={queryClient}>
@@ -64,13 +66,12 @@ describe("MovieForm tests", () => {
                 </Router>
             </QueryClientProvider>
         );
-        expect(
-            await screen.findByTestId(`${testId}-cancel`)
-        ).toBeInTheDocument();
+        expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
         const cancelButton = screen.getByTestId(`${testId}-cancel`);
 
         fireEvent.click(cancelButton);
 
         await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
     });
+
 });
